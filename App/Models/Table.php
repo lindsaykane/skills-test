c34daebbe131fed0c1969f0ec4bb3057bc6f98f3
lindsaykane <39602047+lindsaykane@users.noreply.github.com>
2018-05-25 09:22:45 -0400

    sort array descending by lname

--- App/Models/Table.php
+++ App/Models/Table.php
@@ -10,10 +10,35 @@ class Table extends \Core\Model
 
         $getFileContents = json_decode(file_get_contents($file['fileToUpload']['tmp_name']), true);
 
+        $getFileContents = $this->array_msort($getFileContents, array('LName'=>SORT_DESC));
+
         if(is_array($getFileContents)) {
             $result['fileContents'] = $getFileContents;
         }
 
         return $result;
+    }
+
+    private function array_msort($array, $cols) {
+        $colarr = array();
+        foreach ($cols as $col => $order) {
+            $colarr[$col] = array();
+            foreach ($array as $k => $row) { $colarr[$col]['_'.$k] = strtolower($row[$col]); }
+        }
+        $eval = 'array_multisort(';
+        foreach ($cols as $col => $order) {
+            $eval .= '$colarr[\''.$col.'\'],'.$order.',';
+        }
+        $eval = substr($eval,0,-1).');';
+        eval($eval);
+        $ret = array();
+        foreach ($colarr as $col => $arr) {
+            foreach ($arr as $k => $v) {
+                $k = substr($k,1);
+                if (!isset($ret[$k])) $ret[$k] = $array[$k];
+                $ret[$k][$col] = $array[$k][$col];
+            }
+        }
+        return $ret;
     }
 }
